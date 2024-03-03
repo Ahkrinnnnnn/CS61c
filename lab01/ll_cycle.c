d0ed571a0ea13a6d1302e0df45fa770dfe1c75d3
Ahkrinnnnnn <3162310027@qq.com>
2024-03-03 17:26:27 +0800

    Finished lab01

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,18 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    if(!head) {
+	return 0;
+    }
+    node *tortoise = head;
+    node *hare = head;
+    while(hare) {
+	hare = hare->next ? 
+		hare->next->next : NULL;
+	tortoise = tortoise->next;
+	if(tortoise == hare) {
+		return 1;
+	}
+    }
     return 0;
 }
