19be767a9dba2006bc15a0cd51b1cb6c35317399
Ahkrinnnnnn <3162310027@qq.com>
2024-03-15 18:04:46 +0800

    Finished lab03

--- lab03/factorial.s
+++ lab03/factorial.s
@@ -22,3 +22,34 @@ main:
 
 factorial:
     # YOUR CODE HERE
+
+    # j recursive
+
+    j iterative
+
+recursive:  # para a0, ret_val a1; So comment out the line 12 when use
+    beq a0, x0, exit_rec
+    addi sp, sp, -8
+    sw ra, 4(sp)
+    sw a0, 0(sp)
+    addi a0, a0, -1
+    jal recursive
+    lw a0, 0(sp)
+    lw ra, 4(sp)
+    addi sp, sp, 8
+    mul a1, a1, a0
+    jr ra
+exit_rec:
+    li a1, 1
+    jr ra
+
+iterative:  # para a1, ret_val a0
+    li t1, 2
+    blt a0, t1, exit
+    addi a1, a0, -1
+loop:
+    mul a0, a0, a1
+    addi a1, a1, -1
+    bne a1, x0, loop
+exit:
+    jr ra
